5a89758095fe18cb38d0d5f6a9a54d8ba113ffd8
Francisco Ahumada <franciscotahumada@gmail.com>
2021-07-28 00:14:04 -0400

    Generar interfaz

--- web/zgames/resources/views/registrar_consolas.blade.php
+++ web/zgames/resources/views/registrar_consolas.blade.php
@@ -1,37 +1,7 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.master')
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <header>
-      <nav class="navbar navbar-expand-lg navbar-light bg-warning">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#"><img src="{{asset('img/z.png')}}" 
-            class="img-fluid" height="50" width="50"></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-link active" aria-current="page" href="{{route('registrar_consolas')}}">Registrar Consola</a>
-              <a class="nav-link" href="{{route('registrar_juego')}}">Registrar Juego</a>
-              <a class="nav-link" href="{{route('ver_consolas')}}">Ver Consolas</a>
-              <a class="nav-link" href="{{route('ver_juegos')}}">Ver Juegos</a>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </header>
-    <main class="container-fluid">
-      <div class="row mt-5">
+@section('contenido')
+    <div class="row mt-5">
         <div class="col-12 col-md-8 col-lg-5 mx-auto">
           <div class="card">
             <div class="card-header bg-warning">
@@ -39,8 +9,8 @@
           </div>
           <div class="card-body">
             <div class="mb-3">
-              <label for="consola-txt" class="form-label">Nombre</label>
-              <input type="text" class="form-control" id="consola-txt">
+              <label for="nombre-txt" class="form-label">Nombre</label>
+              <input type="text" class="form-control" id="nombre-txt">
             </div>
             <div class="mb-3">
               <label for="marca-select" class="form-label">Marca</label>
@@ -52,17 +22,14 @@
               </select>
             </div>
             <div class="mb-3">
-              <label for="anio-num" class="form-label">Año</label>
-              <input type="number" class="form-control" id="anio-num">
+              <label for="anio-txt" class="form-label">Año de lanzamiento</label>
+              <input type="number" class="form-control" id="anio-txt">
             </div>
           </div>
           <div class="card-footer d-grid gap-1">           
-            <button type="button" class="btn btn-success">Agregar</button>
+            <button type="button" class="btn btn-info">Agregar</button>
           </div>
           </div>
         </div>
       </div>
-    </main>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-  </body>
-</html>
+@endsection
--- web/zgames/resources/views/ver_consolas.blade.php
+++ web/zgames/resources/views/ver_consolas.blade.php
@@ -1,12 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-     Hola soy ver_consolas
-</body>
-</html>
+@extends('layouts.master')
+
+@section('contenido')
+    <div class="row mt-5">
+        <div class="col-12 col-md-12 col-lg-6 mx-auto">
+            <table class="table table-bordered table-hover table-striped table-responsive">               
+                <thead class="bg-info text-dark">
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Marca</th>
+                        <th>Año de lanzamiento</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody id="tbody-tabla"></tbody>
+            </table>
+        </div>
+    </div>
+@endsection
