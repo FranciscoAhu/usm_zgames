dc168284bea70bca0a9377cc4c91e1308e624704
Francisco Ahumada <franciscotahumada@gmail.com>
2021-07-28 19:18:37 -0400

    Generar modelo y tabla para consola

--- web/zgames/app/Http/Controllers/ConsolasController.php
+++ web/zgames/app/Http/Controllers/ConsolasController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Consola;
 use Illuminate\Http\Request;
 
 class ConsolasController extends Controller
@@ -14,5 +14,24 @@ class ConsolasController extends Controller
         $marcas[]= "Sega";
 
         return $marcas;
+    }
+    //busca las consolas que existen en la bd y las retorna
+    public function getConsolas(){
+        //equivalente a select * from consolas  
+        $consolas = Consola::all();
+        return $consolas;
+    }
+
+    //registra una consola de ejemplo
+    public function crearConsola(){
+        //Equivalente a un insert
+        $consola = new Consola();
+        $consola->nombre = "Nintendo Switch";
+        $consola->marca = "Nintendo";
+        $consola->anio = 2015;
+
+        $consola->save();
+
+        return $consola;
     }
 }
