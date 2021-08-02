4d95d6700dce8599b04bb458c73ab9e3f9cb9a9c
Francisco Ahumada <franciscotahumada@gmail.com>
2021-08-02 17:53:03 -0400

    Agregar controlador de juegos y Modelo de Juegos

--- web/zgames/app/Models/Consola.php
+++ web/zgames/app/Models/Consola.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Consola extends Model
 {
+
     use HasFactory;
+
+    public function  juegos(){
+        return $this->hasMany("App\Models\Juego","consola_id");
+    }
 }
